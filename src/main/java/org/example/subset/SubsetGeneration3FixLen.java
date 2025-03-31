b168a9a5493cfeb229679242c570fd44911cca52
Andrey Hlushakou <andrey.hlushakou@gmail.com>
2025-03-31 16:54:09 +0300

    SubsetGeneration3FixLen added subsetsWithoutRes

--- src/main/java/org/example/subset/SubsetGeneration3FixLen.java
+++ src/main/java/org/example/subset/SubsetGeneration3FixLen.java
@@ -10,12 +10,13 @@ public class SubsetGeneration3FixLen {
     private SubsetGeneration3FixLen() {}
 
     public static void main(String[] args) {
-        System.out.println("SubsetGeneration3Backtracking");
-        int lenSet = 30; // Длина множества
-        int lenSubset = 10; // ТОЛЬКО подмножества длины 10
+        System.out.println("SubsetGeneration3FixLen");
+        int lenSet = 31; // Длина множества
+        int lenSubset = 14; // ТОЛЬКО подмножества длины lenSubset
 
         List<Integer> original = getListByLen(lenSet);
-        calculationTimeAndPrintfResult(() -> subsets(original, lenSubset));
+//        calculationTimeAndPrintfResult(() -> subsets(original, lenSubset));
+        calculationTime.accept(() -> subsetsWithoutRes(original, lenSubset));
     }
 
     static <T> List<List<T>> subsets(List<T> original, int lenSubset) {
@@ -36,6 +37,26 @@ public class SubsetGeneration3FixLen {
         for (int j = i; j < original.size(); j++) {
             subset.add(original.get(j));
             subsetRecur(j + 1, original, res, subset, lenSubset);
+            subset.remove(subset.size() - 1); // Убираем последний элемент (backtracking)
+        }
+    }
+
+    static <T> void subsetsWithoutRes(List<T> original, int lenSubset) {
+        List<T> subset = new ArrayList<>(lenSubset);
+        subsetRecurWithoutRes(0, original, subset, lenSubset);
+    }
+
+    static <T> void subsetRecurWithoutRes(int i, List<T> original, List<T> subset, int lenSubset) {
+        // Если нашли ровно lenSubset элементов — добавляем результат
+        if (subset.size() == lenSubset) {
+//            res.add(new ArrayList<>(subset));
+            return;
+        }
+
+        // Перебираем оставшиеся элементы
+        for (int j = i; j < original.size(); j++) {
+            subset.add(original.get(j));
+            subsetRecurWithoutRes(j + 1, original, subset, lenSubset);
             subset.remove(subset.size() - 1); // Убираем последний элемент (backtracking)
         }
     }
